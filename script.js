f87a672492ebc98538b7fbf4cc317346d9adb4b5
Ritwik Singh <76203605+ritwikhero@users.noreply.github.com>
2024-04-01 22:23:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function firstChar(text) {
   // your code here
 	if(text.length == 0){
-		return ' ';
+		return " ";
 	}
 	text = text.trim();
 
 	return text[0];
 }
+
 
 // Do not change the code below
 const text = prompt("Enter text:");
